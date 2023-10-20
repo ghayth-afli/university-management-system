ce80c6b7a03794052948c0c55be1c29276eb0590
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2023-10-20 10:27:00 +0100

    first commit

---
+++ server/src/main/java/com/soa/uneversity/management/system/UneversityManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.soa.uneversity.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UneversityManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UneversityManagementSystemApplication.class, args);
+	}
+
+}
