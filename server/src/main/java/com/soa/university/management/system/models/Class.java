f3418ac97be567cfd02d15fffe78cf3c3c145e78
oussama boufari <oussamaboufari@gmail.com>
2023-10-21 18:00:24 +0100

    fixes

--- server/src/main/java/com/soa/university/management/system/models/Class.java
+++ server/src/main/java/com/soa/university/management/system/models/Class.java
@@ -1,7 +1,15 @@
 package com.soa.university.management.system.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@AllArgsConstructor
+@Getter
+@Setter
 public class Class {
-    
+    private Long Id;
+    private String name;
+    private int nbrStudents;
+
 }
--- server/src/main/java/com/soa/university/management/system/models/Teacher.java
+++ server/src/main/java/com/soa/university/management/system/models/Teacher.java
@@ -9,4 +9,6 @@ import lombok.Setter;
 @Setter
 public class Teacher {
     private Long Id;
+    private String img;
+   
 }
