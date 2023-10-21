944e8c057e1b5c76dad5a521f74e25b200cba87c
Manel BELHADJ <belhaj343@gmail.com>
2023-10-21 21:18:02 +0100

    removed unecessary attributes

--- client/src/pages/authentication/auth-forms/AuthLogin.js
+++ client/src/pages/authentication/auth-forms/AuthLogin.js
@@ -156,14 +156,6 @@ const AuthLogin = () => {
                   </Button>
                 </AnimateButton>
               </Grid>
-              <Grid item xs={12}>
-                <Divider>
-                  <Typography variant="caption"> Login with</Typography>
-                </Divider>
-              </Grid>
-              <Grid item xs={12}>
-                <FirebaseSocial />
-              </Grid>
             </Grid>
           </form>
         )}
