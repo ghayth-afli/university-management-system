c67af194f7c51e8915a112254c9c7a7d677519e6
Manel BELHADJ <belhaj343@gmail.com>
2023-11-03 23:52:10 +0100

    dashboard error fix

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -92,43 +92,45 @@ const DashboardDefault = () => {
   }, []);
 
   return (
-    <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-      {/* row 1 */}
-      <Grid item xs={12} sx={{ mb: -2.25 }}>
-        <Typography variant="h5">Dashboard</Typography>
-      </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <AnalyticEcommerce title="Total Administrators" count={data.nbOfAdministrators} percentage={32.3} />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <AnalyticEcommerce title="Total Teachers" count={data.nbOfTeachers} percentage={5} />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <AnalyticEcommerce title="Total Students" count="530" percentage={69.2} />
-      </Grid>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <AnalyticEcommerce title="Total Absences" count={data.nbOfAbsences} percentage={27.4} isLoss color="warning" />
-      </Grid>
-
-      <Grid item md={8} sx={{ display: { sm: 'none', md: 'block', lg: 'none' } }} />
-
-      {/* row 2 */}
-      <Grid item xs={12} md={7} lg={12}>
-        <Grid container alignItems="center" justifyContent="space-between">
-          <Grid item>
-            <Typography variant="h5">Absences per classe</Typography>
-          </Grid>
-          <Grid item>
-            <Stack direction="row" alignItems="center" spacing={0}></Stack>
-          </Grid>
+    data && (
+      <Grid container rowSpacing={4.5} columnSpacing={2.75}>
+        {/* row 1 */}
+        <Grid item xs={12} sx={{ mb: -2.25 }}>
+          <Typography variant="h5">Dashboard</Typography>
         </Grid>
-        <MainCard content={false} sx={{ mt: 1.5 }}>
-          <Box sx={{ pt: 1, pr: 2 }}>
-            <IncomeAreaChart slot={slot} />
-          </Box>
-        </MainCard>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <AnalyticEcommerce title="Total Administrators" count={data.nbOfAdministrators} percentage={32.3} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <AnalyticEcommerce title="Total Teachers" count={data.nbOfTeachers} percentage={5} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <AnalyticEcommerce title="Total Students" count={data.nbOfStudents} percentage={69.2} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={3}>
+          <AnalyticEcommerce title="Total Absences" count={data.nbOfAbsences} percentage={27.4} isLoss color="warning" />
+        </Grid>
+
+        <Grid item md={8} sx={{ display: { sm: 'none', md: 'block', lg: 'none' } }} />
+
+        {/* row 2 */}
+        <Grid item xs={12} md={7} lg={12}>
+          <Grid container alignItems="center" justifyContent="space-between">
+            <Grid item>
+              <Typography variant="h5">Absences per classe</Typography>
+            </Grid>
+            <Grid item>
+              <Stack direction="row" alignItems="center" spacing={0}></Stack>
+            </Grid>
+          </Grid>
+          <MainCard content={false} sx={{ mt: 1.5 }}>
+            <Box sx={{ pt: 1, pr: 2 }}>
+              <IncomeAreaChart slot={slot} />
+            </Box>
+          </MainCard>
+        </Grid>
       </Grid>
-    </Grid>
+    )
   );
 };
 
